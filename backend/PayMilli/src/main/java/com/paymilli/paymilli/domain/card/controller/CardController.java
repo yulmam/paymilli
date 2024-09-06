e5fdcd28e73aa26d5815c08697c097262f7df774
yulmam <yulmam1@naver.com>
2024-09-06 17:44:36 +0900

    feat: cardController 저장

--- backend/PayMilli/src/main/java/com/paymilli/paymilli/domain/card/controller/CardController.java
+++ backend/PayMilli/src/main/java/com/paymilli/paymilli/domain/card/controller/CardController.java
@@ -1,6 +1,8 @@
 package com.paymilli.paymilli.domain.card.controller;
 
 
+import com.paymilli.paymilli.domain.card.dto.request.AddCardRequest;
+import com.paymilli.paymilli.domain.card.service.CardService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +12,18 @@ import org.springframework.web.bind.annotation.*;
 public class CardController {
 
     private final CardService cardService;
+    private final webCl
     public CardController(CardService cardService){
         this.cardService = cardService;
     }
 
     @PostMapping
     public ResponseEntity<?> addCard(@RequestBody AddCardRequest addCardRequest){
+        if(!cardService.isPresent(addCardRequest))
+            return ResponseEntity.notFound();
+        if(!cardService.isAleadyRegister(cardService.getId()))
+            return ResponseEntity.badRequest();
+        if(!cardService.)
         return ResponseEntity.ok();
     }
 
