d9061ea296688460f0bd508ea5a1b120642e4ff4
yulmam <yulmam1@naver.com>
2024-09-07 21:13:04 +0900

    feat: card도메인 service 추가

--- backend/PayMilli/src/main/java/com/paymilli/paymilli/domain/card/service/CardService.java
+++ backend/PayMilli/src/main/java/com/paymilli/paymilli/domain/card/service/CardService.java
@@ -1,4 +1,19 @@
 package com.paymilli.paymilli.domain.card.service;
 
+import com.paymilli.paymilli.domain.card.dto.request.AddCardRequest;
+import com.paymilli.paymilli.domain.card.dto.response.CardResponse;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface CardService {
+    public boolean isAlreadyRegister(String cardNumber, UUID userId);
+
+    public void checkValidation(AddCardRequest addCardRequest);
+
+    public void registerCard(AddCardRequest addCardRequest, UUID userId);
+
+    public List<CardResponse> searchCards(UUID userId);
+
+    public void deleteCard(UUID cardId, UUID userId);
 }
--- backend/PayMilli/src/main/java/com/paymilli/paymilli/domain/card/service/CardServiceImpl.java
+++ backend/PayMilli/src/main/java/com/paymilli/paymilli/domain/card/service/CardServiceImpl.java
@@ -1,7 +1,12 @@
 package com.paymilli.paymilli.domain.card.service;
 
+import com.paymilli.paymilli.domain.card.dto.request.AddCardRequest;
+import com.paymilli.paymilli.domain.card.dto.response.CardResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class CardServiceImpl implements CardService{
@@ -11,9 +16,32 @@ public class CardServiceImpl implements CardService{
 
     public CardServiceImpl(WebClient webClient, CardRepository cardRepository){
         this.webClient  = webClient;
-        this.cardRepository = cardRepository
+        this.cardRepository = cardRepository;
     }
 
 
+    @Override
+    public boolean isAlreadyRegister(String cardNumber, UUID userId) {
+        return false;
+    }
 
+    @Override
+    public void checkValidation(AddCardRequest addCardRequest) {
+
+    }
+
+    @Override
+    public void registerCard(AddCardRequest addCardRequest, UUID userId) {
+
+    }
+
+    @Override
+    public List<CardResponse> searchCards(UUID userId) {
+        return List.of();
+    }
+
+    @Override
+    public void deleteCard(UUID cardId, UUID userId) {
+
+    }
 }
