a3b97d46ba3e77f86389605abd1331ce74f11853
yulmam <yulmam1@naver.com>
2024-09-13 13:36:18 +0900

    chore: edit controller
    <body>
    
    <footer>

--- backend/CardCompany/src/main/java/com/cardcompany/cardcompany/TestController.java
+++ backend/CardCompany/src/main/java/com/cardcompany/cardcompany/TestController.java
@@ -1,9 +1,11 @@
 package com.cardcompany.cardcompany;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test")
+@RestController
+@RequestMapping("/test")
 public class TestController {
 
 	@GetMapping("/1")
