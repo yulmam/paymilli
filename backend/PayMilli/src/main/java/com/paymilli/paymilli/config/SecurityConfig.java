7e05ec6bdcdc1616166f36b36313db0348e8edde
yulmam <yulmam1@naver.com>
2024-09-20 12:49:59 +0900

    fix: h2 관련 코드 제거 및 설정파일 수정

--- backend/PayMilli/src/main/java/com/paymilli/paymilli/config/SecurityConfig.java
+++ backend/PayMilli/src/main/java/com/paymilli/paymilli/config/SecurityConfig.java
@@ -5,13 +5,10 @@ import com.paymilli.paymilli.domain.member.jwt.JwtAuthenticationEntryPoint;
 import com.paymilli.paymilli.domain.member.jwt.JwtFilter;
 import com.paymilli.paymilli.domain.member.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -59,13 +56,6 @@ public class SecurityConfig {
         ;
 
         return http.build();
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
-    public WebSecurityCustomizer configureH2ConsoleEnable() {
-        return web -> web.ignoring()
-            .requestMatchers(PathRequest.toH2Console());
     }
 
 
