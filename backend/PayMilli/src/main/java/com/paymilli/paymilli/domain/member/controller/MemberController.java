8a18da7bc0e66ac35087eab6b3171a158e8fdfa1
yulmam <yulmam1@naver.com>
2024-09-20 13:33:46 +0900

    refactor: refresh token cookie 생성 기능 분리

--- backend/PayMilli/src/main/java/com/paymilli/paymilli/domain/member/controller/MemberController.java
+++ backend/PayMilli/src/main/java/com/paymilli/paymilli/domain/member/controller/MemberController.java
@@ -54,12 +54,7 @@ public class MemberController {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + tokenResponse.getAccessToken());
 
-        Cookie refreshTokenCookie = new Cookie("refreshToken", tokenResponse.getRefreshToken());
-        refreshTokenCookie.setHttpOnly(true); // JavaScript에서 쿠키 접근을 막음
-        refreshTokenCookie.setSecure(true);   // HTTPS를 통해서만 전송되도록 설정
-        refreshTokenCookie.setPath("/");      // 쿠키가 전체 도메인에서 사용될 수 있도록 설정
-        refreshTokenCookie.setMaxAge(7 * 24 * 60 * 60); // 쿠키의 유효 기간을 7일로 설정 (필요에 따라 조정)
-
+        Cookie refreshTokenCookie = generateRefreshTokenCookie(tokenResponse.getRefreshToken());
         response.addCookie(refreshTokenCookie);
 
         memberService.addRefreshToken(loginRequest.getMemberId(), tokenResponse.getRefreshToken());
@@ -112,10 +107,20 @@ public class MemberController {
         @RequestBody UpdatePaymentPasswordRequest updatePaymentPasswordRequest) {
 
         String accessToken = tokenProvider.extractAccessToken(token);
-        String memberId = tokenProvider.getMemberId(token);
+        String memberId = tokenProvider.getMemberId(accessToken);
 
         memberService.updatePaymentPassword(memberId, updatePaymentPasswordRequest);
 
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private Cookie generateRefreshTokenCookie(String refreshToken) {
+        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+        refreshTokenCookie.setHttpOnly(true); // JavaScript에서 쿠키 접근을 막음
+        refreshTokenCookie.setSecure(true);   // HTTPS를 통해서만 전송되도록 설정
+        refreshTokenCookie.setPath("/");      // 쿠키가 전체 도메인에서 사용될 수 있도록 설정
+        refreshTokenCookie.setMaxAge(7 * 24 * 60 * 60); // 쿠키의 유효 기간을 7일로 설정 (필요에 따라 조정)
+
+        return refreshTokenCookie;
     }
 }
