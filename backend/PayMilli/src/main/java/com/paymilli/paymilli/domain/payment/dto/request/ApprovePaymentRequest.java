c3b1bf12761564e6d6aa5d197ac95b34c1d2763c
yulmam <yulmam1@naver.com>
2024-09-27 16:34:03 +0900

    hotfix: dto 생성자  추가

--- backend/PayMilli/src/main/java/com/paymilli/paymilli/domain/payment/dto/request/ApprovePaymentRequest.java
+++ backend/PayMilli/src/main/java/com/paymilli/paymilli/domain/payment/dto/request/ApprovePaymentRequest.java
@@ -1,8 +1,10 @@
 package com.paymilli.paymilli.domain.payment.dto.request;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ApprovePaymentRequest {
 
     private String password;
